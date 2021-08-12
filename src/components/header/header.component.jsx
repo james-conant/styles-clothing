4f2fbec10ab553b920f7a8405be8dbaea645b280
James Conant <jconant1993@gmail.com>
2021-08-11 22:18:21 -0400

    Adds redirect if user is signed in

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -13,24 +13,26 @@ const Header = ({ currentUser }) => (
     <Link className="logo-container" to="/">
       <Logo className="logo" />
     </Link>
-    <div className="options">
-      <Link className="option" to="/shop">
-        SHOP
-      </Link>
-    </div>
-    <div className="options">
-      <Link className="option" to="/shop">
-        CONTACT
-      </Link>
-      {currentUser !== null ? (
-        <div className="option" onClick={() => auth.signOut()}>
-          SIGN OUT
-        </div>
-      ) : (
-        <Link className="option" to="/signin">
-          SIGN IN
+    <div className="options__wrapper">
+      <div className="options">
+        <Link className="option" to="/shop">
+          SHOP
         </Link>
-      )}
+      </div>
+      <div className="options">
+        <Link className="option" to="/shop">
+          CONTACT
+        </Link>
+        {currentUser !== null ? (
+          <div className="option" onClick={() => auth.signOut()}>
+            SIGN OUT
+          </div>
+        ) : (
+          <Link className="option" to="/signin">
+            SIGN IN
+          </Link>
+        )}
+      </div>
     </div>
   </div>
 );
