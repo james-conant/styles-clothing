c56379201a54de044fa0bfb81845389e3c988af8
James Conant <jconant1993@gmail.com>
2021-08-15 17:05:52 -0400

    Adds checkout page, checkout-item component, updates cart with selectors

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -7,10 +7,24 @@ export const selectCartItems = createSelector(
   (cart) => cart.cartItems
 );
 
+export const selectCartHidden = createSelector(
+  [selectCart],
+  (cart) => cart.hidden
+);
+
+// redux already has a shallow equality check for the integer and wont re-render but keeping reduce function here.
 export const selectCartItemsCount = createSelector(
   [selectCartItems],
   (cartItems) =>
     cartItems.reduce((accumilatedQuantity, cartItem) => {
       return accumilatedQuantity + cartItem.quantity;
     }, 0)
+);
+
+export const selectCartTotal = createSelector(
+  [selectCartItems], 
+  (cartItems) =>
+  cartItems.reduce((accumilatedTotal, cartItem) => {
+    return accumilatedTotal + cartItem.price * cartItem.quantity;
+  }, 0)
 );
