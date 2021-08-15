2f9a3a0f8aa9e48cbef4ba415afbd6bfab325219
James Conant <jconant1993@gmail.com>
2021-08-14 20:20:19 -0400

    Adds reselect package and uses memoized selectors for quantity in cart-dropdown and cart-item components.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { connect } from "react-redux";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 
 import CustomButton from "../custom-button/custom-buttom.component";
 import CartItem from "../cart-item/cart-item.component";
@@ -21,8 +22,8 @@ const CartDropdown = ({ cartItems }) => {
   );
 };
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state),
 });
 
 export default connect(mapStateToProps)(CartDropdown);
